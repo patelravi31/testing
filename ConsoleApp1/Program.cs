c057645f3bcde2d44a4dfb13f7c96002c2acd335
ravi patel <rp311286@gmail.com>
2017-10-16 13:04:49 +0530

    jjj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,10 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow world"); Console.WriteLine("sdkskd");
+            Console.WriteLine("Hellow "); Console.WriteLine("sdkskd");
+            int a = 1;
+
+
             Console.WriteLine("Hello");
         }
     }
