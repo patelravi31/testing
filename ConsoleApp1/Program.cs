9568b757f79e8bb571ad2e58a552e83b3c658949
ravi patel <rp311286@gmail.com>
2017-10-16 13:00:33 +0530

    cmoot

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hellow world");
+            Console.WriteLine("Hellow world"); Console.WriteLine("sdkskd");
             Console.WriteLine("Hello");
         }
     }
